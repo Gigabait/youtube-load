67d885680b0ba98c14c37802aee499317ae93df7
Potatofactory <mrpotatofactory@gmail.com>
2016-04-07 06:59:44 -0500

    Initial Commit

---
+++ config.php
@@ -0,0 +1,113 @@
+<?
+	$steam_key = "EB47486975BD8BF884E376969C0BD2D8"; // YOU NEED THIS
+	
+	$banner_top = "Welcome to YouTube Load"; // What it will say on the top
+	
+	$PlaylistGenerationSize = 10; // How many videos we should generate and queue
+	
+	//////////////////////////////////////////////////////
+	// Name: Static List		                        //
+	// Desc: Ignores randomizer and plays only 1 video  //
+	//////////////////////////////////////////////////////
+	function static_list( $id ) {
+		$list = array(
+		
+		//	ID 					|  VideoID
+			"76561197972597160" => "i3uVX-RUFVw",
+			"76561198060486309" => "_s8OlKt_dRY",
+			
+		);
+		
+		if ( in_array( $id, $list ) ) { return false; }
+		return $list[$id];
+	}
+	
+	//////////////////////////////////////////////////////
+	// Name: YouTube Playlist                           //
+	// Desc: Randomizes videos based on embedded ID     //
+	//////////////////////////////////////////////////////
+	
+	function GetRandomVideo() {
+		$videolist = array(
+			"mN6O62qwyGY",
+			"PDu_T54K-VI",
+			"OFzXaFbxDcM",
+			"hgKDu5pp_fU",
+			"D1sZ_vwqwcE",
+			"48jt_lWA7W0",
+			"b6vSf0cA9qY",
+			"e-fA-gBCkj0",
+			"vKYAbhIEZug",
+			"KD5fLb-WgBU",
+			"IgGjUjQRAxw",
+			"5-6fEj17MK4",
+			"E7fzUGR8ZH4",
+			"scmQ6iN8hKM",
+			"6fdr-Fiv92c",
+			"vc3JWo2iiGc",
+			"l18A5BOTlzE",
+			"gk-aCL6eyGc",
+			"n4tK7LYFxI0",
+			"oABEGc8Dus0",
+			"dGghkjpNCQ8",
+			"ojofvfeA3Fg",
+			"WomOv0gB7O8",
+			"kn6-c223DUU",
+			"8PTDv_szmL0",
+			"aTBSQKh8teE",
+			"_ovdm2yX4MA",
+			"Sv6dMFF_yts",
+			"ghb6eDopW8I",
+			"sf6LD2B_kDQ",
+			"dVVZaZ8yO6o",
+			"36reZ9-3VK0",
+			"IJNR2EpS0jw",
+			"PWofgnLXTtk",
+			"RlD-Sufzrh4",
+			"4u-6B_iF5s0",
+			"8rnr2wuAhYc",
+			"y6Sxv-sUYtM",
+			"Jj-pSzqb-Xk",
+			"c71RCAyLS1M",
+			"6qwkVGDKVvE",
+			"WZJKIFWFSU0",
+			"gTLWb9BJbiI",
+			"ytc0U2WAz4s",
+			"GNpNfhpqDk4",
+			"HAIDqt2aUek",
+			"qUsm3PCoGIE",
+			"5NV6Rdv1a3I",
+			"hT_nvWreIhg",
+			"qrOeGCJdZe4",
+			"ktvTqknDobU",
+			"dvgZkm1xWPE",
+			"YgFyi74DVjc",
+			"jlAgHt92lqE",
+			"-RYlAPjyNm8",
+		);
+		
+		return $videolist[rand( 0, count( $videolist ) - 1 )]; // Don't touch this!!
+	}
+	
+	//////////////////////////////////////////////////////
+	// Name: Automatic Playlist Generator               //
+	// Desc: I recommend not touching this.             //
+	//////////////////////////////////////////////////////
+	
+	function GeneratePlaylist() {
+		while ($x <= $GLOBALS['PlaylistGenerationSize']) {
+			$VideoID = GetRandomVideo();
+			if( strpos($PlaylistQueue , $VideoID) != false) {  // If we get a duplicate in the playlist
+				// print("<script>console.warn(\"Duplicate found: " . $VideoID . ". REQUESTED RETRY!!\");</script>");
+			} else {
+				if( $x != $GLOBALS['PlaylistGenerationSize'] ) {
+					$PlaylistQueue .= $VideoID . ",";
+				} else {
+					$PlaylistQueue .= $VideoID;
+				}
+				$x++;
+			}
+		}
+		return $PlaylistQueue;
+	}
+?>
---
+++ index.php
@@ -0,0 +1,91 @@
+<?
+	include "config.php";
+	$id = $_GET["id"];
+	if( $steam_key != "Get this from http://steamcommunity.com/dev/apikey") {
+		$playerarray = json_decode(file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' . $steam_key . '&steamids=' . $id . '&format=json'), true);
+		$username = htmlspecialchars($playerarray['response']['players'][0]['personaname']); // Username
+	} else {
+		Header( "Location: http://steamcommunity.com/dev/apikey" );
+	}
+?>
+<html>
+	<head>
+		<meta charset="UTF-8" />
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,600,400&subset=cyrillic,cyrillic-ext,latin,greek-ext,greek,latin-ext,vietnamese' rel='stylesheet' type='text/css'>
+		<style>
+			* {
+				pointer-events: none;
+				width: 100%;
+				height: 100%;
+				margin: 0px;
+				padding: 0px;
+				overflow: hidden;
+				background-color: black;
+			}
+			
+			h1 {
+				width: 100%;
+				font-size: 36pt;
+				height: 7%;
+				font-weight: 100;		
+				padding: 12px 0px;
+				color: white;
+				font-family: 'Open Sans', sans-serif;
+				text-align: center;
+			}
+			.footer {
+				position: fixed;
+				bottom: 7px;
+				width: 100%;
+				z-index: 2;		
+				padding-top: 20px;
+				background-color: brown;
+			}
+			
+			.title {
+				background: url(img/background.png) repeat;
+				padding-bottom: 20px;
+				z-index: 2;	
+			}
+			.overlay {
+				position: fixed;
+				background: url(img/overlays/1.png);
+				left: 0px;
+				top: 0px;
+				width: 100%;
+				height: 100%;
+				z-index: 1;
+				background-repeat: repeat;
+			}
+		</style>
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script>
+			$(document).ready(function() {
+				$("#footer").slideDown(0, function() {
+					$(this).delay(5000).slideUp('slow').delay(10000).animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+				});
+				$("#title").slideUp(0, function() {
+					$(this).delay(5000).slideDown('slow').delay(10000).animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+				});  
+			});
+		</script>
+	</head>
+	
+	<body>
+		<div class="overlay"></div>
+		<h1 id="title" class="title"><? echo($banner_top); ?></h1>
+		<h1 id="footer" class="footer">Howdy, <?php echo($username); ?></h1>
+		
+		<div id="video">
+			<iframe id="player" width="100%" height="100%" src="//www.youtube.com/embed/<? print( GetRandomVideo() ); ?>?iv_load_policy=3&rel=0&autoplay=1&loop=1&showinfo=0&controls=0&playlist=<? print( GeneratePlaylist() ); ?>" frameborder="0" allowfullscreen></iframe>
+		</div>
+	</body>
+	<?
+		if( static_list( $id ) != false ){
+			print("<script>document.getElementById(\"video\").innerHTML = \"<iframe width='100%' height='100%' src='//www.youtube.com/embed/" . static_list( $id ) . "?iv_load_policy=3&rel=0&autoplay=1&showinfo=0&controls=0' frameborder='0' allowfullscreen></iframe>\"</script>\"");
+		}
+	?>
+	<script>
+		document.getElementById("player").volume=0.2; // Controls volume
+	</script>
+</html>
