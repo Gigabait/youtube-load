e5b11221e22239a683a2f5bca15514617f64df17
Potatofactory <mrpotatofactory@gmail.com>
2016-05-08 20:04:26 -0500

    Updated to our latest loading screen.
    -- Killed the cloud system
    
    (It's still somewhat operational if you really wanna use it tho)

--- config.php
+++ config.php
@@ -1,11 +1,65 @@
-<?
-	error_reporting(0);
+<?php
+	//error_reporting(0);
 
 	$steam_key = "Get this from http://steamcommunity.com/dev/apikey"; // YOU NEED THIS
 	
 	$banner_top = "Welcome to Pococraft"; // What it will say on the top
 	
 	$PlaylistGenerationSize = 20; // How many videos we should generate and queue
+
+	$videolist = array(
+		"0O2aH4XLbto",
+		"BfSX7EN0yXs",
+		"L7fQoXzIgeg",
+		"17svtURunUk",
+		"hyj4JFSErrw",
+		"YlEb3L1PIco",
+		"lhjk5x54bsE",
+		"IU5vKbyc3c8",
+		"XYmLhn3qJ6Y",
+		"2vjPBrBU-TM",
+		"VtQxK5TQ22g",
+		"_04s7ONnJRs",
+		"9UpunuBr4QI",
+		"C2cr7SBBT6A",
+		"_4kuQ_T95VY",
+		"IcrbM1l_BoI",
+		"-fm8VahT1hs",
+		"qYodWEKCuGg",
+		"QYVwqJCIdR0",
+		"GNpNfhpqDk4",
+		"y6oXW_YiV6g",
+		"y6oXW_YiV6g",
+		"2Z4m4lnjxkY",
+		"ZZ5LpwO-An4",
+		"gy1B3agGNxw",
+		"6md5RSnVUuo",
+		"elsh3J5lJ6g",
+		"eE9tV1WGTgE",
+		"LWGJA9i18Co",
+		"UJKythlXAIY",
+		"3xUfCUFPL-8",
+		"c-5nR_xMpj4",
+		"TAK5uw7x-Aw",
+		"AI_RDJ7ifZQ",
+		"Kybyy1EkoOE",
+		"vmOArLdg9d4",
+		"CNlFkP6nvLE",
+		"GnrwM7vFn_U",
+		"IvUU8joBb1Q",
+		"gHCDN7Gr3KU",
+		"Vn1pf0Xi3nU",
+		"r_oaVD4NzYo",
+		"JvQY6tLMdL0",
+		"11t75Vsgjc0",
+		"bwsrbC51jxc",
+		"QbZfY28ptv4",
+		"7PdV2UtsNes",
+		"ZGpXlL5Ltdc",
+		"RX6YeYT3ed8",
+		"we3MSqEISSU",
+	);
+
 	//////////////////////////////////////////////////////
 	// Name: Static List		                        //
 	// Desc: Ignores randomizer and plays only 1 video  //
@@ -26,60 +80,8 @@
 	// Desc: Randomizes videos based on embedded ID     //
 	//////////////////////////////////////////////////////
 	
-	function GetRandomVideo() {
-		$videolist = array(
-			"0O2aH4XLbto",
-			"BfSX7EN0yXs",
-			"L7fQoXzIgeg",
-			"17svtURunUk",
-			"hyj4JFSErrw",
-			"YlEb3L1PIco",
-			"lhjk5x54bsE",
-			"IU5vKbyc3c8",
-			"XYmLhn3qJ6Y",
-			"2vjPBrBU-TM",
-			"VtQxK5TQ22g",
-			"_04s7ONnJRs",
-			"9UpunuBr4QI",
-			"C2cr7SBBT6A",
-			"_4kuQ_T95VY",
-			"IcrbM1l_BoI",
-			"-fm8VahT1hs",
-			"qYodWEKCuGg",
-			"QYVwqJCIdR0",
-			"GNpNfhpqDk4",
-			"y6oXW_YiV6g",
-			"y6oXW_YiV6g",
-			"2Z4m4lnjxkY",
-			"ZZ5LpwO-An4",
-			"gy1B3agGNxw",
-			"6md5RSnVUuo",
-			"elsh3J5lJ6g",
-			"eE9tV1WGTgE",
-			"LWGJA9i18Co",
-			"UJKythlXAIY",
-			"3xUfCUFPL-8",
-			"c-5nR_xMpj4",
-			"TAK5uw7x-Aw",
-			"AI_RDJ7ifZQ",
-			"Kybyy1EkoOE",
-			"vmOArLdg9d4",
-			"CNlFkP6nvLE",
-			"GnrwM7vFn_U",
-			"IvUU8joBb1Q",
-			"gHCDN7Gr3KU",
-			"Vn1pf0Xi3nU",
-			"r_oaVD4NzYo",
-			"JvQY6tLMdL0",
-			"11t75Vsgjc0",
-			"bwsrbC51jxc",
-			"QbZfY28ptv4",
-			"7PdV2UtsNes",
-			"ZGpXlL5Ltdc",
-			"RX6YeYT3ed8",
-		);
-		
-		return $videolist[mt_rand( 0, count( $videolist ) )]; // Don't touch this!!
+	function GetRandomVideo() {		
+		return $GLOBALS['videolist'][mt_rand( 0, sizeof( $GLOBALS['videolist'] ) - 1 )]; // Don't touch this!!
 	}
 	function GeneratePlaylist() {
 		
--- index.php
+++ index.php
@@ -1,40 +1,67 @@
-<?
+<?php
 	include "config.php";
+	include "lib/steamuser.class.php";
 	if( isset($_GET["id"]) ) {
 		$id = $_GET["id"];
 	} else {
 		die( "#ERROR: Missing 'id' argument" );
 	}
 
+	$json = new UserJson();
+	//$json->master = $json->Read();
+
 	if( $steam_key != "Get this from http://steamcommunity.com/dev/apikey") {
 		$playerarray = json_decode(file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=' . $steam_key . '&steamids=' . $id . '&format=json'), true);
 		if( !$playerarray ) {
 			error_reporting(0);
-			$username = "{poot name here cuz steam is down :(}";
+			$json->properties['name'] = "Player";
+			$json->properties['avatar'] = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/7c/7cfdd5970fa46b9784f16369a9464b3b0d409008_full.jpg";
+			$json->properties['group'] = "user";
 		} else {
-			$username = htmlspecialchars($playerarray['response']['players'][0]['personaname']); // Username
+			if (is_null($playerarray['response']['players'][0]['personaname'])) {
+				$json->properties['name'] = "Pirate";
+				$json->properties['avatar'] = "https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/7c/7cfdd5970fa46b9784f16369a9464b3b0d409008_full.jpg";
+				$json->properties['group'] = "user";
+			} else {
+				$json->properties['name'] = htmlspecialchars($playerarray['response']['players'][0]['personaname']); // Username
+				$json->properties['avatar'] = htmlspecialchars($playerarray['response']['players'][0]['avatarfull']); // Username
+				//if (isset($json->master[$json->SteamWorksDecode64($id)]['group'])) {
+				//	$json->properties['group'] = $json->master[$json->SteamWorksDecode64($id)]['group'];
+				//} else {
+				//	$json->properties['group'] = "user";
+				//}
+				
+			}           
 		}
-		
 	} else {
 		Header( "Location: http://steamcommunity.com/dev/apikey" );
 	}
+
+	//if (isset($json->properties['group'])) {
+	//	if (isset($json->phrases[$json->properties['group']])) {
+	//		$json->properties['welcometext'] = $json->phrases[$json->properties['group']];
+	//	} else {
+	//		$json->properties['welcometext'] = $json->phrases["~DEFAULT"];
+	//	}
+	//} else {
+	//	$json->properties['welcometext'] = $json->phrases["~DEFAULT"];
+	//}
+	$json->properties['welcometext'] = "It's gonna take a while to load in :|";
+
 ?>
+<!DOCTYPE html>
 <html>
 	<head>
+		<title>YouTube Load (Potato Version)</title>
 		<meta charset="UTF-8" />
-		<link href='<? if ($_SERVER['HTTPS']) { 
-			print("http://fonts.googleapis.com/css?family=Open+Sans:600,400");
-		} else {
-			print("https://fonts.googleapis.com/css?family=Open+Sans:600,400");
-		}?>' rel='stylesheet' type='text/css'>
 		<style>
 			@font-face {
 				font-family: VCR;
 				src: url(../2014-mas/fonts/vcr.woff);
 			}
 			@font-face {
-				font-family: PCFont;
-				src: url(../2014-mas/fonts/pcsenior.ttf);
+				font-family: coolvetica;
+				src: url(../2014-mas/fonts/coolvetica.ttf);
 			}
 			* {
 				pointer-events: none;
@@ -53,27 +80,41 @@
 				height: 7%;
 				font-weight: 100;
 				color: white;
-				text-align: center;
+			}
+
+			#video {
+				background: none;
+			}
+
+			#avatar {
+				width: 128px;
+			    height: 128px;
+			    border-radius: 100%;
+			    background: none;
 			}
 			.footer {
+				height: auto;
+				left: 2%;
 				position: fixed;
-				bottom: 7px;
+				bottom: 2%;
 				width: 100%;
-				z-index: 2;		
+				z-index: 2;     
 				padding-top: 20px;
-				padding-bottom: 10px;
-				background-color: rgba(0, 0, 0, 0);
+				padding-bottom: 20px;
+				background: none;
 			}
 			.post_footer { /* $(this).toggleClass('post_footer'); */
 				background-color: green;
 			}
 			.title {
+				text-align: center;
+				display: none;
 				background: url(img/header.png);
 				padding-bottom: 5px;
 				padding-top: 20px;
-				z-index: 3;	
+				z-index: 3; 
 			}
-			#overlay {
+			#container2 {
 				position: fixed;
 				left: 0px;
 				top: 0px;
@@ -95,37 +136,15 @@
 				background-image: url(img/overlays/static.gif);
 			}
 			
-			.container {
+			.overlay {
 				position: fixed;
-				background: url(img/overlays/1.png) repeat;
-				left: 0px;
-				top: 0px;
-				width: 100%;
-				height: 100%;
-				z-index: 1;
-				opacity: .15;
-			}
-			.container2 {
-				position: fixed;
-				background: url(img/overlays/1.png) repeat;
+				background: url(img/overlays/2.png) repeat;
 				left: 0px;
 				top: 0px;
 				width: 100%;
 				height: 100%;
 				z-index: 2;
-				opacity: 0.15;
-			}
-			
-			.footer_alt { /* $(this).toggleClass('clicked'); */
-				-webkit-animation: rainbow 10s infinite;
-				-ms-animation: rainbow 10s infinite;
-				-o-animation: rainbow 10s infinite;
-				animation: rainbow 10s infinite;
-				background-color: rgba(0, 0, 0, 0);
-				font-family: PCFont, 'Open Sans', sans-serif;
-				z-index: 5;
-				margin-top: 6px;
-				font-size: 10pt;
+				opacity: 0.5;
 			}
 			
 			@-webkit-keyframes rainbow {
@@ -183,33 +202,18 @@
 		</style>
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script>
-			$(document).ready(function() {
-				$("#footer").slideDown(0, function() {
-					$(this).delay(5000).slideUp('slow').delay(10000).animate({ bottom: '0%', opacity: 'toggle', }, 1000);
-				});
-				$("#title").slideUp(0, function() {
-					$(this).delay(5000).slideDown('slow').delay(10000).animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
-				});  
-			});
-			
 			function SetStatusChanged( status ) {
 				if ( status == "Workshop Complete" ) {
-					$(".footer_alt").html( "Looks like you finished downloading our workshop content, On to on-board content!" )
+					// um
 				} else if ( status == "Sending client info..." ) {
-					document.getElementById("player").volume=0.05; 
-					$(".footer_alt").html( "You're done downloading all our content<br /><br />Now it's finally time to actually play on our gameserver!" )
-					$(".footer_name").remove()
 					$(".static").fadeIn( 7000 ) // Fade to static
-					var footer = $("#footer");
-					var footer_alt = $(".footer_alt");
-					footer_alt.css("animation", "initial");
-					footer_alt.css("font-size", "14pt");
-					footer_alt.css("color", "yellow");
-					footer.css("position","absolute");
-					footer.css("top", Math.max(0, (($(window).height() - footer.outerHeight()) / 2) + $(window).scrollTop()) + "px");
-					footer.css("left", Math.max(0, (($(window).width() - footer.outerWidth()) / 2) + $(window).scrollLeft()) + "px");
 				}
 			}
+			setTimeout(function() {
+				$("#footer").animate({ left: '5%', opacity: 'toggle', }, 500).delay(10000).animate({ left: '2%', opacity: 'toggle', }, 1000);
+				$("#video").animate({ '-webkit-filter': 'blur(10px)', filter: 'blur(10px)' }, 500).delay(10000).animate({ '-webkit-filter': 'blur(0px)', filter: 'blur(0px)', }, 1000);
+				$("#title").slideDown('slow').delay(10000).animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
+			}, 5000);
 		</script>
 	</head>
 	
@@ -217,24 +221,25 @@
 		<!--<audio hidden autoplay name="media">
 			<source src="welcome.mp3" type="audio/mpeg">
 		</audio>-->
-		<div class="container2"></div>
-		<div id="overlay">
-			<h1 id="title" class="title"><? echo($banner_top); ?></h1>
+		<div class="overlay"></div>
+		<div id="container2">
+			<h1 id="title" class="title"><?php echo($banner_top); ?></h1>
 			<h1 id="footer" class="footer">
-				<div class="footer_name" style="background: none; height: auto;">Hey <?php echo($username); ?></div>
-				<div class="footer_alt">You are now loading in, This might take a while!</div><
-			/h1>
+				<img id="avatar" src="<?php echo($json->properties['avatar']); ?>"/>
+				<span style="font-family: 'coolvetica', arial; background: none;height: auto;position: absolute;padding: 10px 14px;"><?php echo($json->properties['name']); ?></span>
+				<span style="font-family: 'coolvetica', arial; font-size: 18pt;background: none;height: auto;position: absolute;padding: 60px 14px;"><?php echo($json->properties['welcometext']); ?></span>
+			</h1>
 		</div>
 		<div id="static" class="static">
 		</div>
 		<div id="video">
-			<iframe id="player" width="100%" height="100%" src="https://www.youtube.com/embed/<? print( GetRandomVideo() ); ?>?iv_load_policy=3&rel=0&autoplay=1&loop=1&showinfo=0&controls=0&playlist=<? print( GeneratePlaylist() ); ?>" frameborder="0" allowfullscreen></iframe>
+			<iframe id="player" width="100%" height="100%" src="https://www.youtube.com/embed/<?php print( GetRandomVideo() ); ?>?iv_load_policy=3&rel=0&autoplay=1&loop=1&showinfo=0&controls=0&playlist=<?php print( GeneratePlaylist() ); ?>" frameborder="0" allowfullscreen></iframe>
 		</div>
 		<script>
 			document.getElementById("player").volume=0.2; // Volume control
 		</script>
 	</body>
-	<?
+	<?php
 		if( static_list( $id ) != false ){
 			print("<script>document.getElementById(\"video\").innerHTML = \"<iframe width='100%' height='100%' src='//www.youtube.com/embed/" . static_list( $id ) . "?iv_load_policy=3&rel=0&autoplay=1&showinfo=0&controls=0' frameborder='0' allowfullscreen></iframe>\"</script>\"");
 		}
